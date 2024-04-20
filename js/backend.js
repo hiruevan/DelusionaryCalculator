5aa56a713f7b6a8d6602674241c6de95bb8fdb80
QuasarOrb <105732138+QuasarOrb@users.noreply.github.com>
2024-04-20 09:23:56 -0600

    Update

--- js/backend.js
+++ js/backend.js
@@ -61,38 +61,3 @@ function calculate() {
 
 
 
-
-
-// Keys that are allowed to add to the calculation
-const allowedKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '(', ')', '*', '/', '+', '-', '!', '^', '%', '.', 'x', 'y', 'z', 'a', 'b', 'c', 't', 'h', 'k', 'd', 'i','Delete','Backspace'];
-let pressingCtr = false;
-
-// Add expressions if user types
-window.addEventListener('keydown', function(e) {
-    console.log(e.key);
-    e.preventDefault();
-    if (e.key == "Control") {
-        pressingCtr = true;
-    }
-    if (e.key == "c" && pressingCtr) {
-        pressingCtr = false;
-        clearDisplay();
-        return;
-    }
-    if (e.key == "Delete" || e.key == "Backspace") {
-        removeLastLetter();
-        return;
-    }
-
-    if (allowedKeys.includes(e.key)) {
-        appendToDisplay(e.key);
-    }
-    if (e.key == "Enter" || e.key == "=") {
-        calculate();
-    }
-    
-});
-
-window.addEventListener('keyup', function(e) {
-    if (e.key == 'Control') pressingCtr = false;
-});
