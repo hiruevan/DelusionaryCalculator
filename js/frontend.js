9a3d50ee22b03ccad50d83d202e1b8ddf9898e67
Hiruevan <98787662+hiruevan@users.noreply.github.com>
2024-04-19 19:13:36 -0600

    Update
    Fix Smoot's JS (Not entirely, but better now) and update CSS

--- js/frontend.js
+++ js/frontend.js
@@ -1,3 +1,5 @@
+// This file is where all user interaction is.
+
 
 const observer = new IntersectionObserver((entries)=>{entries.forEach((entry)=> {
         console.log(entry)
@@ -17,4 +19,32 @@ const observer = new IntersectionObserver((entries)=>{entries.forEach((entry)=> 
 
 
 const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach((el) => observer.observe(el))
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Keys that are allowed to add to the calculation
+const allowedKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '(', ')', '*', '/', '+', '-', '!', '^', '%', '.', 'x', 'y', 'z', 'a', 'b', 'c', 't', 'h', 'k', 'd', 'i'];
+let pressingCtr = false;
+
+// Add expressions if user types
+window.addEventListener('keydown', function(e) {
+    console.log(e.key);
+    e.preventDefault();
+    if (e.key == "Control") {
+        pressingCtr = true;
+    }
+    if (e.key == "c" && pressingCtr) {
+        pressingCtr = false;
+        clearDisplay();
+        return;
+    }
+    if (allowedKeys.includes(e.key)) {
+        appendToDisplay(e.key);
+    }
+    if (e.key == "Enter" || e.key == "=") {
+        calculate();
+    }
+});
+
+window.addEventListener('keyup', function(e) {
+    if (e.key == 'Control') pressingCtr = false;
+});
