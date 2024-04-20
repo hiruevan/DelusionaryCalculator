bdb8cdcd5a08a814b59820be69f8a415837fddce
QuasarOrb <105732138+QuasarOrb@users.noreply.github.com>
2024-04-20 08:50:53 -0600

    Added backspace support
    moved key input to backend,js

--- js/backend.js
+++ js/backend.js
@@ -1,5 +1,5 @@
 // This file connects the js to python (In addition to setting up some display functions)
-
+let currentDisplayText = "<br>"
 // Function to append a character to the display
 function appendToDisplay(value) {
     if (document.getElementById('display').innerHTML == 'Error') clearDisplay();
@@ -8,9 +8,15 @@ function appendToDisplay(value) {
     }
     document.getElementById('display').innerHTML += value;
 }
+function removeLastLetter() {
+    currentDisplayText = document.getElementById('display').innerHTML
+    currentDisplayText = currentDisplayText.replace(/.$/, '')
+    document.getElementById('display').innerHTML = currentDisplayText
+}
 
 // Function to clear the display
 function clearDisplay() {
+    
     document.getElementById('display').innerHTML = '<br>';
 }
 
@@ -38,3 +44,50 @@ function calculate() {
         document.getElementById('display').innerHTML = 'Error';
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Keys that are allowed to add to the calculation
+const allowedKeys = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '(', ')', '*', '/', '+', '-', '!', '^', '%', '.', 'x', 'y', 'z', 'a', 'b', 'c', 't', 'h', 'k', 'd', 'i','Delete','Backspace'];
+let pressingCtr = false;
+
+// Add expressions if user types
+window.addEventListener('keydown', function(e) {
+    console.log(e.key);
+    e.preventDefault();
+    if (e.key == "Control") {
+        pressingCtr = true;
+    }
+    if (e.key == "c" && pressingCtr) {
+        pressingCtr = false;
+        clearDisplay();
+        return;
+    }
+    if (e.key == "Delete" || e.key == "Backspace") {
+        removeLastLetter();
+        return;
+    }
+
+    if (allowedKeys.includes(e.key)) {
+        appendToDisplay(e.key);
+    }
+    if (e.key == "Enter" || e.key == "=") {
+        calculate();
+    }
+    
+});
+
+window.addEventListener('keyup', function(e) {
+    if (e.key == 'Control') pressingCtr = false;
+});
